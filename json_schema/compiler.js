1863170a29cb852f0898ed9afa492f13c1b2320b
tom lark <tom.larkworthy@gmail.com>
2014-03-26 22:23:45 +0000

    initial draft

---
+++ json_schema/compiler.js
@@ -0,0 +1,28 @@
+var js_yaml = require("js-yaml");
+var fs  = require("fs");
+var tv4 = require("tv4");
+
+
+
+var json = js_yaml.load(fs.readFileSync("example.yaml").toString());
+
+console.log(json);
+
+
+var schema = json.schema;
+
+console.log("schema:");
+console.log(schema);
+
+var banUnknownProperties = true;
+var checkRecursive = true;
+var meta_schema = JSON.parse(fs.readFileSync("core_meta_schema.json").toString());
+tv4.addSchema("http://json-schema.org/draft-04/schema", meta_schema);
+var valid = tv4.validate(schema, meta_schema, checkRecursive, banUnknownProperties);
+
+console.log("isValid?", valid);
+if(!valid){
+    console.log(tv4.error)
+}
+console.log("tv4.missing?", tv4.missing);
+console.log("tv4.getMissingUris?", tv4.getMissingUris());
---
+++ node_modules/posix-clock/binding.gyp
@@ -0,0 +1,8 @@
+{
+  "targets": [
+    {
+      "target_name": "posix-clock",
+      "sources": ["src/posix-clock.cpp"]
+    }
+  ]
+}
