7a19626e4614aa95c4770a9b3e157f0da8af3276
Tom Larkworthy <larkworthy@google.com>
2014-12-23 13:55:02 -0800

    extra test

--- test/scenarios_test.ts
+++ test/scenarios_test.ts
@@ -12,7 +12,7 @@ import tv4 = require("tv4");
 
 function run() {
     //called if this file is run, used to enable runtime debugging
-    console.log("working?", checkScenario("./test/scenarios/minimum1.yaml"))
+    console.log("working?", checkScenario("./test/scenarios/function4.yaml"))
 }
 
 var checkScenario = function(path: string): boolean {
